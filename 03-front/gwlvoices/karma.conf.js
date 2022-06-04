db0cb3897a03f090eb493f6ef163cabef0d37493
jlarostegui <luis.garcia.arostegui@gmail.com>
2022-06-04 19:03:06 +0200

    creado archivo de angular

--- 03-front/gwlvoices/karma.conf.js
+++ 03-front/gwlvoices/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gwl'),
+      dir: require('path').join(__dirname, './coverage/gwlvoices'),
       subdir: '.',
       reporters: [
         { type: 'html' },
